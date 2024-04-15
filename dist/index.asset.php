2f7150c8cd1b8a80484ad1110f8ac06512cdbd5f
Monir <fencemonir@gmail.com>
2024-04-15 10:56:11 +0600

    Fixed: compatibility support with wordpress 6.5 version

--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3a1022de200df24dfeac');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b33460a8a70dd0997d2e');
